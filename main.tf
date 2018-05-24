d2dce83d4d4346368459e21fd3e8e3ab9fc43a6f
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2018-05-24 13:27:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.11.1"
 }
 
 provider "aws" {
@@ -10,7 +10,11 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-
+  key_name = "${var.key_name}"
+  associate_public_ip_address = "true"
+  root_block_device {
+    volume_size = "${var.volume_size}"
+  }
   tags {
     Name = "${var.name}"
   }

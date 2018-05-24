a4a1512a31f9424618b67b515cf4420d331749c7
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2018-05-24 14:33:00 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ variable "aws_region" {
 }
 
 variable "ami_id" {
-  description = "ID of the AMI to provision. Consul Build of Ubuntu 16.04"
-  default = "ami-022f81c5506e84bc9"
+  description = "ID of the AMI to provision. CentOS Linux 7"
+  default = "ami-e1496384"
 }
 
 variable "instance_type" {

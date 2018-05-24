d4a37a0def7fe438f7e32c5e3f49df835b677747
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2018-05-24 13:15:53 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-east-1"
+  default = "us-west-1"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. This is a Nomad-AWS-Terraform-AMI"
-  default = "ami-357cc74a"
+  default = "ami-2e1ef954"
 }
 variable "instance_type" {
   description = "type of EC2 instance to provision."
@@ -16,3 +16,12 @@ variable "name" {
   description = "name to pass to Name tag"
   default = "Provisioned by Terraform"
 }
+
+variable "owner" {
+  description = "Name to pass to the Owner tag"
+  default = "ajennings"
+}
+
+variable "ttl" { 
+  description = "Hours until instances are reaped by N.E.P.T.R"
+  default = "3"

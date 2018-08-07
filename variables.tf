353b000d196fbb8534b7193af81e27243d654ac9
Amanda Jennings <38109573+agjenni80@users.noreply.github.com>
2018-08-06 23:08:50 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-9bc8f2fe"
+  default = "ami-2e1ef954"
 }
 
 variable "instance_type" {

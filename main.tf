3272d046794f91dc08cabc0b7f2117c8090d8e10
Amanda Jennings <ajennings@hashicorp.com>
2019-03-25 17:53:45 -0400

    chef change

--- main.tf
+++ main.tf
@@ -15,7 +15,41 @@ resource "aws_instance" "web" {
   root_block_device {
     volume_size = "${var.volume_size}"
   }
+ 
+ provisioner "chef" {
+
+     connection {
+           type        = "ssh"
+           user        = "azureuser"
+           private_key = "${var.private_key}"
+   
+}
+   attributes_json = <<-EOF
+      {
+        "key": "value",
+        "app": {
+          "cluster1": {
+            "nodes": [
+              "chef-terraform"
+            ]
+          }
+        }
+      }
+    EOF
+
+    environment     = "ajennings_test"
+    run_list        = ["recipe[chef_terraform_azure_demo]"]
+    node_name       = "chef-terraform"
+    server_url      = "https://api.chef.io/organizations/ajennings"
+    recreate_client = true
+    fetch_chef_certificates = true
+    user_name       = "ajennings"
+    user_key        = "${var.key_material}"
+    version         = "12.21.1"
+    # If you have a self signed cert on your chef server change this to :verify_none
+    ssl_verify_mode = ":verify_none"
+}
   tags {
-    Name = "${var.name}"
+    environment = "Terraform AWS Chef Demo"
   }
 }

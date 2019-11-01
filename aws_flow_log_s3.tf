3663246a1f718dd3b071f19faa75f1aadc8da132
Amanda Jennings <ajennings@hashicorp.com>
2019-11-01 14:26:23 -0400

    update arn

--- aws_flow_log_s3.tf
+++ aws_flow_log_s3.tf
@@ -1,5 +1,5 @@
 resource "aws_flow_log" "hc-sec-logs" {
-  log_destination      = "${arn:aws:s3:::hc-sec-flow-logs}"
+  log_destination      = "${https://s3.console.aws.amazon.com/s3/buckets/hc-sec-flow-logs/}"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = "${vpc-0a317ca1447094027}"

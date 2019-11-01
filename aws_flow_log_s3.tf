e7ef4734ba4e1ba76c1ee0b76a5c2647e4edc88f
Amanda Jennings <ajennings@hashicorp.com>
2019-11-01 14:07:41 -0400

    update

--- aws_flow_log_s3.tf
+++ aws_flow_log_s3.tf
@@ -1,4 +1,4 @@
-resource "aws_flow_log" "example" {
+resource "aws_flow_log" "hc-sec-logs" {
   log_destination      = "${https://s3.console.aws.amazon.com/s3/buckets/hc-sec-flow-logs}"
   log_destination_type = "s3"
   traffic_type         = "ALL"
